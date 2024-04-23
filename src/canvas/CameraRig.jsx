c7efdfcdacce2ff1b9dc625776f1f0a2612be949
mFaYizp <fayizfaz78@gmail.com>
2024-04-23 20:12:56 +0530

    backdrop and camera rig setted

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -1,8 +1,9 @@
-import { useFrame } from "@react-three/fiber";
-import { easing } from "maath";
-import { useSnapshot } from "valtio";
-import state from "../store";
-import { useRef } from "react";
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const CameraRig = ({ children }) => {
   const group = useRef();
@@ -12,28 +13,30 @@ const CameraRig = ({ children }) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
+    // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
-    if (snap.intro) {
-      if (isBreakpoint) targetPosition = [0, 0, 2];
-
-      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    if(snap.intro) {
+      if(isBreakpoint) targetPosition = [0, 0, 2];
+      if(isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
-      if (isMobile) targetPosition = [0, 0, 2.5];
+      if(isMobile) targetPosition = [0, 0, 2.5]
       else targetPosition = [0, 0, 2];
     }
 
-    //model camera position
-    easing.dampE(state.camera.position, targetPosition, 0.25, delta);
+    // set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
 
-    //model rotation
+    // set the model rotation smoothly
     easing.dampE(
       group.current.rotation,
-      [state.pointer.y / 10, -state.pointer.x / 10, 0],
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
       0.25,
       delta
-    );
-  });
-  return <group ref={group}>{children}</group>;
-};
+    )
+  })
 
-export default CameraRig;
+
+  return <group ref={group}>{children}</group>
+}
+
+export default CameraRig
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,21 +1,25 @@
 import { Center, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Shirt from "./Shirt";
-import BackDrop from "./BackDrop";
 import CameraRig from "./CameraRig";
-import { AmbientLight } from "three";
+import BackDrop from "./Backdrop";
 
 const CanvasModal = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
       <CameraRig>
-        {/* <BackDrop />  */}
+        <BackDrop />
         <Center>
           <Shirt />
-        </Center> 
+        </Center>
       </CameraRig>
     </Canvas>
   );
